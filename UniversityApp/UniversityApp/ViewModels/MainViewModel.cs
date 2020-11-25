53a0a7748c50151a28265777119340f6e3733665
dsantafe <santafedavid01@gmail.com>
2020-11-24 20:34:33 -0500

    HomePage and HomeViewModel

--- UniversityApp/UniversityApp/ViewModels/MainViewModel.cs
+++ UniversityApp/UniversityApp/ViewModels/MainViewModel.cs
@@ -7,6 +7,7 @@
         public MoviesViewModel Movies { get; set; }
         public CreateCourseViewModel CreateCourse { get; set; }
         public LoginViewModel Login { get; set; }
+        public HomeViewModel Home { get; set; }
 
         public MainViewModel()
         {
@@ -14,6 +15,8 @@
             CourseInstructors = new CourseInstructorsViewModel();
             CreateCourse = new CreateCourseViewModel();
             Login = new LoginViewModel();
+            Home = new HomeViewModel();
+
             //Movies = new MoviesViewModel();
         }
     }
